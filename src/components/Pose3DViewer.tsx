0b0197e90e246a30454a352a30b5ba5df0c8ff84
ypyo99 <141600271+ypyo99@users.noreply.github.com>
2026-08-06 09:15:29 +0900

    Match 3D angle text to 2D photo mode (Bold, size, right-aligned)

--- src/components/Pose3DViewer.tsx
+++ src/components/Pose3DViewer.tsx
@@ -203,13 +203,14 @@ const Pose3DViewer: React.FC<Pose3DViewerProps> = ({ worldLandmarks, poseLandmar
         
         {/* Draw Angles using WebGL Text so they appear in screenshots */}
         {anglesToRender.map((angle, i) => (
-          <Billboard key={`angle-${i}`} position={[angle.pos[0], angle.pos[1] + 0.3, angle.pos[2]]}>
+          <Billboard key={`angle-${i}`} position={[angle.pos[0] + 0.3, angle.pos[1], angle.pos[2]]}>
             <Text
-              fontSize={0.1}
+              font="https://fonts.gstatic.com/s/roboto/v30/KFOlCnqEu92Fr1MmWUlfBBc4.woff"
+              fontSize={0.25}
               color="white"
-              outlineWidth={0.015}
+              outlineWidth={0.04}
               outlineColor="black"
-              anchorX="center"
+              anchorX="left"
               anchorY="middle"
             >
               {angle.text}
